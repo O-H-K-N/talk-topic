092ecd08c99439ba6a665c5e6933779e50aa49db
techkento <kent0tsuka1118@gmail.com>
2022-02-21 11:38:50 +0900

    Add：バリデーションの設定とコントローラーの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :account_name, presence: true, uniqueness: true, length: { in: 4..20 }
+  validates :password, length: { in: 8..20 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
