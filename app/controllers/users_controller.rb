6d81aa5996e28b2d12b0be19e6bfb78aafe69847
techkento <kent0tsuka1118@gmail.com>
2022-02-21 15:26:33 +0900

    Add：ユーザー登録、ログイン機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login
+  # before_action :set_user, only: :new
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :account_name,
+      :password,
+      :password_confirmation
+    )
+  end
 end
