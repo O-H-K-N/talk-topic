4b3f76b7f5ae27118c9426bb31df52a013aa446b
techkento <kent0tsuka1118@gmail.com>
2022-02-22 23:54:49 +0900

    Fix：モデルのリファクタリングの修正

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,8 @@
 class Topic < ApplicationRecord
-  has_many :tags, through: :topic_tags
   has_many :topic_tags, dependent: :destroy
+  has_many :tags, through: :topic_tags
   validates :user_name, presence: true
   validates :user_name, length: { in: 2..20 }, allow_blank: true
   validates :title, presence: true, length: { maximum: 255}
+  validates :tag_ids, presence: true
 end
