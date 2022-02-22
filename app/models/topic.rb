36d78772c269e1e70756ca0ef12decdda17409c3
techkento <kent0tsuka1118@gmail.com>
2022-02-22 17:05:40 +0900

    Fix：バリデーション設定を修正

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ApplicationRecord
-  validates :user_name, presence: true, allow_blank: true
-  validates :user_name, length: { in: 2..20 }
+  validates :user_name, presence: true
+  validates :user_name, length: { in: 2..20 }, allow_blank: true
   validates :title, presence: true, length: { maximum: 255}
 end
