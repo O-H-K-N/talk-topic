1ed205dbf8117af2c68a760e4ec48e0dba7f5d2a
techkento <kent0tsuka1118@gmail.com>
2022-02-23 21:13:35 +0900

    Fix：DB名を変更・不要なDBを削除

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_22_081043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
