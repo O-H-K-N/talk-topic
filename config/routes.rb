9e5a036677b8bf6e10178c531a8cced2f6eea0d8
techkento <kent0tsuka1118@gmail.com>
2022-02-23 21:18:13 +0900

    Remove：新たにユーザが作成されないようにするために、ユーザー新規作成のルーティングを除去

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "homes#index"
 
-  resources :users
   resources :topics do
     collection do
       get 'tags'
@@ -12,6 +11,4 @@ Rails.application.routes.draw do
   get '/login', to: 'user_sessions#new', as: :login
   post '/login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy', as: :logout
-  get '/sign_up', to: 'users#new', as: :sign_up
-  post '/sign_up', to: 'users#create'
 end
