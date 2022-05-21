942fbd05d50ddd02c0f2caa4511b61ce671dd5d0
大塚健登 <kent0tsuka1118@gmail.com>
2022-05-21 18:06:43 +0900

    Add：OGP設定

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem 'font-awesome-sass'
 #Japanese support
 gem 'rails-i18n'
 
+#meta-tag
+gem 'meta-tags'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,32 @@ module ApplicationHelper
 		page_title.empty? ? base_title : page_title + " | " + base_title
 
   end
+
+  def default_meta_tags
+    {
+      site: '会話のネタ帳',
+      title: '使える会話のネタを提供してください！',
+      reverse: true,
+      description: '様々な場面に応じて、役に立った会話のネタを共有しあうアプリ',
+      keywords: '会話 話のネタ 飲み会 職場 オンライン 合コン',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('favicon.ico'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary',
+      }
+    }
+  end
 end
